ff5ce8279ab4936d2ddba82caeaea61abb5df1f8
jun1718 <gbeovhsqhtka@naver.com>
2022-12-21 16:17:18 +0900

    Initial commit

---
+++ src/test/java/com/example/jenkins2/Jenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
